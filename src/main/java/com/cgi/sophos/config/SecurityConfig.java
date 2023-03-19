5619fa53db484c6a7d743db937295ee734f77714
naveen94-georgian <naveenselv24@gmail.com>
2023-03-18 22:49:31 -0300

    ci.yml created

--- src/main/java/com/cgi/sophos/config/SecurityConfig.java
+++ src/main/java/com/cgi/sophos/config/SecurityConfig.java
@@ -1,17 +1,12 @@
 package com.cgi.sophos.config;
 
 import com.azure.spring.cloud.autoconfigure.implementation.aad.security.AadResourceServerHttpSecurityConfigurer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.text.MessageFormat;
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
@@ -22,21 +17,26 @@ public class SecurityConfig {
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http.apply(AadResourceServerHttpSecurityConfigurer.aadResourceServer())
                 .and()
-                .csrf().disable()
-                .authorizeHttpRequests().requestMatchers("/swagger-ui/**").permitAll()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/swagger-ui/**")
+                .permitAll()
                 .and()
-                .authorizeHttpRequests().requestMatchers("/ops/**").permitAll()
+                .authorizeHttpRequests()
+                .requestMatchers("/docs/**")
+                .permitAll()
                 .and()
-                .authorizeHttpRequests().anyRequest().authenticated()
+                .authorizeHttpRequests()
+                .requestMatchers("/ops/**")
+                .permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
         return http.build();
-    }
-
-    Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authorizeRequestCustomizer() {
-        return (authorize) -> authorize.requestMatchers("/v1/api/swagger-ui/**").permitAll()
-                .requestMatchers("/ops/**").permitAll()
-                .anyRequest().authenticated();
     }
 }
